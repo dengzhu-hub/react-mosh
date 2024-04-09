f4d5559ad0ffa765f1d0ceb242e39ba597dfde86
dengzhu-hub <3052113768@qq.com>
2024-04-09 19:42:15 +0800

    test the form with name and age

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,21 +1,30 @@
-import { Button } from '../Button';
-import { FormEvent } from 'react';
-import Input from '../Input/Input';
+import { FormEvent, useRef } from 'react';
+
 const Form = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    console.log(inputRef.current?.value);
+
     console.log('Form submitted');
   };
   return (
     <form action="" onSubmit={handleSubmit}>
-      <Input label="description" text="jack" />
-      <Input label="amount" text="" />
-      <Input label="category" text="" />
-      <Button
-        type="submit"
-        onClick={() => console.log('clicked')}
-        children="submit"
-      />
+      <div className="mb-3">
+        <label className="form-label" htmlFor="name">
+          Name
+        </label>
+        <input type="text" className="form-control" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="" className="form-label">
+          Amount
+        </label>
+        <input ref={inputRef} type="number" className="form-control" />
+      </div>
+      <button className="btn btn-primary" type="submit">
+        submit
+      </button>
     </form>
   );
 };
